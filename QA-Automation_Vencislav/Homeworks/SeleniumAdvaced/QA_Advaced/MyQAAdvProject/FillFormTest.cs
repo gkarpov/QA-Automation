98803704bb92ea9bfbcda61846c0fd72aa727b6c
Georgi Karpov <karpov21g@gmail.com>
2019-03-23 15:37:34 +0200

    Added F2 comment

--- QA-Automation_Vencislav/Homeworks/SeleniumAdvaced/QA_Advaced/MyQAAdvProject/FillFormTest.cs
+++ QA-Automation_Vencislav/Homeworks/SeleniumAdvaced/QA_Advaced/MyQAAdvProject/FillFormTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-//Fiature1
+//Fiature2
 namespace MyQAAdvProject
 {
     using MyQAAdvProject.Pages;
